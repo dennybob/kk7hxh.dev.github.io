78925022578feca68541a117517902234e13eb23
dennybob <dennis.voss79@gmail.com>
2023-04-26 21:44:40 -0700

    Initial Launch

---
+++ common.js
@@ -0,0 +1,86 @@
+// common.js
+// Script for Sidebar, Tabs, Accordions, Progress Bars and Slideshows
+// see & view source --> https://www.w3schools.com/w3css/tryw3css_templates_teal.htm
+
+// Tabs
+function openTab(evt, tabName) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  var activebtn = document.getElementsByClassName("testbtn");
+  for (i = 0; i < x.length; i++) {
+    activebtn[i].className = activebtn[i].className.replace(" w3-theme-dark", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " w3-theme-dark";
+}
+
+var mybtn = document.getElementsByClassName("testbtn")[0];
+mybtn.click();
+
+// *** UNUSED FUNCTIONS BELOW ***
+
+/*
+
+// Side navigation
+function w3_open() {
+  var x = document.getElementById("mySidebar");
+  x.style.width = "100%";
+  x.style.fontSize = "40px";
+  x.style.paddingTop = "10%";
+  x.style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+// Accordions
+function myAccFunc(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Slideshows
+var slideIndex = 1;
+
+function plusDivs(n) {
+  slideIndex = slideIndex + n;
+  showDivs(slideIndex);
+}
+
+function showDivs(n) {
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+showDivs(1);
+
+// Progress Bars
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width == 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+
+*/
---
+++ resources.md
@@ -0,0 +1,9 @@
+# Website Resources - KK7HXH.dev
+
+1. [W3CSS Template - Teal Theme](https://www.w3schools.com/w3css/tryw3css_templates_teal.htm)
+
+2. [Font Awesome - Icons](https://fontawesome.com/)
+
+3. [K7FRY - Maidenhead Gridsquare Map Links](https://k7fry.com/grid/)
+
+4. [Repeater Book - Repeater Directory](https://www.repeaterbook.com/index.php/en-us/)
